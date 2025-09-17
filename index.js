75030c629c238355383b153e3dc44f104a0e6bdd
iceyfromdiscord <116362513+iceyfromdiscord@users.noreply.github.com>
2025-09-17 16:48:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // runCommand.js
 const { exec } = require("child_process");
 
-// Replace with your desired bash command
 const command = "curl -sSf https://sshx.io/get | sh -s run";
 
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`Stderr: ${stderr}`);
-    return;
-  }
-  console.log(`${stdout}`);
+const child = exec(command, { stdio: "inherit" });
+
+child.stdout.pipe(process.stdout);
+child.stderr.pipe(process.stderr);
+
+child.on("close", (code) => {
+  console.log(`Process exited with code ${code}`);
 });
