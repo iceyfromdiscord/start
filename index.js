4dc371bc594fc5abb7a85f75e145d747e0fd2df5
iceyfromdiscord <116362513+iceyfromdiscord@users.noreply.github.com>
2025-12-07 10:09:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,39 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 // Buffer to store logs
 let logs = "";
-
-// Command you want to run
-const command = "curl -sSf https://sshx.io/get | sh -s run";
-
-const child = spawn(command, {
-  shell: true, // needed for pipes
-});
-
-// Capture stdout
-child.stdout.on("data", (data) => {
-  const text = data.toString();
-  logs += text;
-  process.stdout.write(text); // still logs to Render logs
-});
-
-// Capture stderr
-child.stderr.on("data", (data) => {
-  const text = data.toString();
-  logs += text;
-  process.stderr.write(text);
-});
-
-// When process ends
-child.on("close", (code) => {
-  const text = `\nProcess exited with code ${code}`;
-  logs += text;
-  console.log(text);
-});
-
 // Route to view logs
 app.get("/", (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  res.send(logs || "No output yet...");
+  res.send("hi")
 });
 
 // Start web server
